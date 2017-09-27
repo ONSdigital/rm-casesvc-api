292f4c5e2cebb93d5da39ffb9165e5ac8b3b8654
Edward Stevens <edward.stevens@ons.gov.uk>
2017-09-27 13:40:57 +0100

    Codacy fixes

--- src/main/java/uk/gov/ons/ctp/response/casesvc/representation/CaseState.java
+++ src/main/java/uk/gov/ons/ctp/response/casesvc/representation/CaseState.java
@@ -1,5 +1,8 @@
 package uk.gov.ons.ctp.response.casesvc.representation;
 
+/**
+ * enum for Case state
+ */
 public enum CaseState {
     ACTIONABLE, INACTIONABLE, REPLACEMENT_INIT, SAMPLED_INIT;
   }
